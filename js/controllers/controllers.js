641c67cb92789bb4e426566714af1cff38e78562
dato <dato39507@yahoo.com>
2015-10-17 14:32:21 +0400

    break into directives

--- js/controllers/controllers.js
+++ js/controllers/controllers.js
@@ -1,10 +1,107 @@
-angular.module('GeoWords', ['$scope']).controller('contentCtrl', 
-function($scope) {
-	$scope.isExpanding = true;
-	$scope.slidePlease = function () {
-		
-        $("#panel").slideToggle("slow");
+var myApp = angular.module('GeoWords', []);
 
+myApp.controller('pageCtrl', ['$scope', function ($scope) {
+
+    $scope.contents = {
+        wildCardContent: {
+            active: 'active',
+            header: 'ვაილდქარდი'
+        },
+        regexContent: {
+            active: '',
+            header: 'რეჯექსი'
+        },
+        rhymeContent: {
+            active: '',
+            header: 'რითმა'
+        }
+    }
+    $scope.currentActiveContent = $scope.contents.wildCardContent;
+
+    $scope.changeCurrentContent = function (choosedContent) {
+        $scope.currentActiveContent.active = '';
+        $scope.currentActiveContent = choosedContent;
+        $scope.currentActiveContent.active = 'active';
+    }
+}]);
+
+myApp.controller('regexCtrl', ['$scope', function ($scope) {
+    $scope.uniqueName = 'regexTemplatePanel';
+    $scope.examples = [{
+        labelStatus: 'success',
+        labelValue: '.',
+        definition: 'წამოიღებს მხოლოდ ერთ ასობგერიან სიტყვებს'
+    }]
+    $scope.searchAction = function (word) {
+
+    }
+}]);
+
+myApp.controller('rhymeCtrl', ['$scope', function ($scope) {
+    $scope.uniqueName = 'rhymeTemplatePanel';
+    $scope.searchAction = function (word) {
+
+    }
+}]);
+
+myApp.controller('wildCardCtrl', ['$scope', function ($scope) {
+    $scope.uniqueName = 'wildCardTemplatePanel';
+    $scope.examples = [{
+        labelStatus: 'success',
+        labelValue: '%',
+        definition: 'წამოიღებს ყველა სიტყვას, რომელშიც ნებისმიერი რაოდენობით ნებისმიერი ბგერაა'
+    }, {
+        labelStatus: 'success',
+        labelValue: 'ველა%',
+        definition: 'წამოიღებს სიტყვებს, რომლებიც იწყება ველა-ზე'
+    }, {
+        labelStatus: 'success',
+        labelValue: '%ველა',
+        definition: 'წამოიღებს სიტყვებს, რომლებიც მთავრდება ველა-ზე'
+    }, {
+        labelStatus: 'success',
+        labelValue: '%ველა%',
+        definition: 'წამოიღებს სიტყვებს, რომლებიც ურევია "ველა"'
+    }, {
+        labelStatus: 'success',
+        labelValue: '_',
+        definition: 'წამოიღებს მხოლოდ ერთ ასობგერიან სიტყვებს '
+    }, {
+        labelStatus: 'success',
+        labelValue: '__',
+        definition: 'წამოიღებს მხოლოდ ორ ასობგერიან სიტყვებს'
+    }, {
+        labelStatus: 'success',
+        labelValue: 'მ_ღლ_ვ_',
+        definition: 'წამოიღებს შვიდასოიან სიტყვებს სადაც 1-ლი, მე-3, მე-4 და მე-6 ასოა მ,ღ,ლ,ვ'
+    }, {
+        labelStatus: 'success',
+        labelValue: '[აეიოუ]%',
+        definition: 'წამოიღებს ყველა სიტყვას რომლის პირველი ასო ხმოვანია'
+    }, {
+        labelStatus: 'success',
+        labelValue: '[ა-დ]%',
+        definition: 'წამოიღებს ყველა სიტყვას რომლის პირველი ასო ა-დ შუალედშია'
+    }, {
+        labelStatus: 'success',
+        labelValue: '[^აეიოუ]%',
+        definition: 'წამოიღებს ყველა სიტყვას რომლის პირველი ასო თანხმოვანია'
+    }, {
+        labelStatus: 'info',
+        labelValue: '%ჯი',
+        definition: 'დომეინების კრეატიული სახელები'
+    }]
+
+    $scope.searchAction = function (word) {
+
+    }
+}]);
+
+
+myApp.controller('examplesController', ['$scope', function ($scope) {
+    $scope.isExpanding = true;
+    $scope.slidePlease = function () {
+        $("#" + $scope.uniqueName).slideToggle("slow");
         if ($scope.isExpanding) {
             $(".examples span").removeClass("glyphicon-chevron-down");
             $(".examples span").addClass("glyphicon-chevron-up")
@@ -15,5 +112,6 @@ function($scope) {
             $(".examples span").addClass("glyphicon-chevron-down");
             $scope.isExpanding = true;
         }
-	}
-});
+    }
+}]);
+
--- js/directives/directives.js
+++ js/directives/directives.js
@@ -1,6 +1,62 @@
-angular.module('GeoWords', []).directive('geoWordsQuery', 
-function() {
-  return {
-    templateUrl: 'js/directives/geoWordsQuery.html'
-  };
+myApp.directive('wildCardContent', function() {
+    return {
+        templateUrl: 'js/directives/geoPageContent.html',
+        controller: "wildCardCtrl",
+        scope:{}
+    };
+});
+
+myApp.directive('regexContent', function () {
+    return {
+        scope:{},
+        templateUrl: 'js/directives/geoPageContent.html',
+        controller: "regexCtrl"
+    };
+});
+
+myApp.directive('rhymeContent', function () {
+    return {
+        scope:{},
+        templateUrl: 'js/directives/geoPageContent.html',
+        controller: "rhymeCtrl"
+    };
+});
+
+
+
+myApp.directive('geoSearchWord', function() {
+    return {
+        templateUrl: 'js/directives/geoSearchInput.html',
+        controller:"searchInputCtrl"
+    };
+});
+
+myApp.directive('geoSearchResult', function() {
+    return {
+        templateUrl: 'js/directives/geoResultTable.html',
+		restrict:'E',
+        scope: {
+           words: '='
+        }
+    };
+})
+
+myApp.directive('geoNavbar', function() {
+    return {
+        templateUrl: 'js/directives/geoNavbar.html'
+    };
+});
+
+
+
+myApp.directive('geoExamplesButton', function() {
+    return {
+        templateUrl: 'js/directives/geoExamplesButton.html',
+        restrict: 'E',
+        scope:{
+            examples: '=',
+            uniqueName:'='
+        },
+		controller:'examplesController'
+    };
 });
